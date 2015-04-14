2d69b9905fa32c3dd11f3e396138eef761f1efa0
sahil <sahilverma1@gmail.com>
2015-04-14 16:56:31 -0700

    Revert

--- BranchMonsterFactory/AppDelegate.m
+++ BranchMonsterFactory/AppDelegate.m
@@ -24,7 +24,49 @@
     // TODO : add initSessionWithLaunchOptions
     // if monster contained in Params, set VC to ViewerVC, else CreatorVC.
     // The deep link handler is called on every install/open to tell you if the user had just clicked a deep link
-    
+    UINavigationController *navController = (UINavigationController *)self.window.rootViewController;
+    NSString * storyboardName = @"Main";
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:storyboardName bundle: nil];
+    UIViewController *nextVC;
+    /*Branch *branch = [Branch getInstance];
+    [branch initSessionWithLaunchOptions:launchOptions andRegisterDeepLinkHandler:^(NSDictionary *params, NSError *error){
+
+        
+        // If the key 'monster' is present in the deep link dictionary
+        // then load the monster viewer with the appropriate monster parameters
+        if ([params objectForKey:@"monster"]) {
+            [MonsterPreferences setMonsterName:[params objectForKey:@"monster_name"]];
+            [MonsterPreferences setFaceIndex:[[params objectForKey:@"face_index"] intValue]];
+            [MonsterPreferences setBodyIndex:[[params objectForKey:@"body_index"] intValue]];
+            [MonsterPreferences setColorIndex:[[params objectForKey:@"color_index"] intValue]];
+            
+            // Choose the monster viewer as the next view controller
+            nextVC = [storyboard instantiateViewControllerWithIdentifier:@"MonsterViewerViewController"];
+            
+            
+        // Else, the app is being opened up from the home screen or from the app store
+        // Load the next logical view controller
+        } else {
+            
+            // If a name has been saved in preferences, then this user has already created a monster
+            // Load the viewer
+            if (![MonsterPreferences getMonsterName]) {
+                [MonsterPreferences setMonsterName:@""];
+                
+                // Choose the monster viewer as the next view controller
+                nextVC = [storyboard instantiateViewControllerWithIdentifier:@"MonsterCreatorViewController"];
+                
+            // If no name has been saved, this user is new, so load the monster maker screen
+            } else {
+                nextVC = [storyboard instantiateViewControllerWithIdentifier:@"MonsterViewerViewController"];
+            }
+        }
+        
+        // launch the next view controller
+    }];*/
+    nextVC = [storyboard instantiateViewControllerWithIdentifier:@"MonsterCreatorViewController"];
+    [navController setViewControllers:@[nextVC] animated:YES];
+
     return YES;
 }
 
@@ -33,6 +75,8 @@
     
     // To receive deep link parameters with the Branch link, you must also call handleDeepLink in the openURL AppDelegate call
     // This will call the deep link handler block you registered above
+    //if (!wasHandled)
+        //[[Branch getInstance] handleDeepLink:url];
     
     return YES;
 }
