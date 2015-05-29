98d44ac27b2fb3afe72afaa0bc82b4a5e34a3d9c
sahil <sahilverma1@gmail.com>
2015-05-28 19:06:45 -0700

    remove unused stubs

--- BranchMonsterFactory/ViewControllers/BranchInfoViewController.m
+++ BranchMonsterFactory/ViewControllers/BranchInfoViewController.m
@@ -14,17 +14,6 @@
 
 @implementation BranchInfoViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-
 #pragma mark - Buttons
 
 -(IBAction)doneTapped:(id)sender {
