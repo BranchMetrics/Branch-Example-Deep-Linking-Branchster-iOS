9c4e9614a694b5b4d3943a6768aa3f496aed6577
echo-branch <43450805+echo-branch@users.noreply.github.com>
2021-08-04 12:14:48 -0700

    CORE-2234 add ATT since apple requires it for apps that indicate that they do track

--- BranchMonsterFactory/AppDelegate.m
+++ BranchMonsterFactory/AppDelegate.m
@@ -85,7 +85,7 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
                     [alertView show];
                     */
                 }
-            }];
+    }];
 
         // Optional: Set our own identitier for this user at Branch.
         // This could be an account number our other userID. It only needs to be set once.
