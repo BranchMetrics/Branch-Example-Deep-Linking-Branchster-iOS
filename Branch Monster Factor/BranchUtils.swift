d5e08ab26f503f36c19096f7bb18642c1c67433d
rob-gioia-branch <robert.gioia@branch.io>
2025-11-05 21:46:29 -0500

    Got custom QR code color updating
    Got custom QR code color updating

--- Branch Monster Factor/BranchUtils.swift
+++ Branch Monster Factor/BranchUtils.swift
@@ -7,17 +7,51 @@
 
 import BranchSDK
 
-func createQRCode(completion: @escaping (UIImage?) -> Void, monsterColor: String, monsterLevel: Int) {
+func createQRCode(
+    completion: @escaping (UIImage?) -> Void, monsterColor: String,
+    monsterLevel: Int
+) {
     let qrCode = BranchQRCode()
-    qrCode.codeColor = UIColor.black
+
+    switch monsterColor {
+    case "green":
+        qrCode.codeColor = UIColor.green
+        break
+    case "red":
+        qrCode.codeColor = UIColor.red
+        break
+    case "blue":
+        qrCode.codeColor = UIColor.blue
+        break
+    case "yellow":
+        qrCode.codeColor = UIColor.yellow
+        break
+    case "purple":
+        qrCode.codeColor = UIColor.purple
+        break
+    case "white":
+        qrCode.codeColor = UIColor.white
+        break
+    case "black":
+        break
+    case "pink":
+        qrCode.codeColor = UIColor.systemPink
+        break
+    case "orange":
+        qrCode.codeColor = UIColor.orange
+        break
+    default:
+        break
+    }
     qrCode.backgroundColor = UIColor.white
     qrCode.width = 1024
     qrCode.margin = 1
     qrCode.imageFormat = .PNG
-    
-    let buo = BranchUniversalObject(canonicalIdentifier: "\(monsterColor)/\(monsterLevel)")
+
+    let buo = BranchUniversalObject(
+        canonicalIdentifier: "\(monsterColor)/\(monsterLevel)")
     let lp = BranchLinkProperties()
-    
+
     qrCode.getAsImage(buo, linkProperties: lp) { qrCodeImage, error in
         if let error = error {
             print("Error creating QR code: \(error.localizedDescription)")
